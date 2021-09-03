3395735d7c728aa9e5749e9b715ee0be7ea779ae
HP <HP@DESKTOP-N5QBMVO>
2021-09-03 20:20:15 +0530

    Add project files.

---
+++ SealedKeyword/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SealedKeyword
+{
+    sealed class SealedClass
+    {
+
+        // Calling Function
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
+    class Program
+    {
+
+        // Main Method
+        static void Main(string[] args)
+        {
+
+            // Creating an object of Sealed Class
+            SealedClass slc = new SealedClass();
+
+            // Performing Addition operation
+            int total = slc.Add(28, 40);
+            Console.WriteLine("Total = " + total.ToString());
+            Console.ReadLine();
+        }
+    }
+}
